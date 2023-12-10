9aa110ddbfbdf03327061522435c60c4f4bb28d6
SabaMuladze <muladze66@bk.ru>
2023-12-10 22:05:39 +0400

    add loading message

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -5,7 +5,8 @@
       <div>
         <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
-      <ul>
+      <p v-if="isLoading">Loading ...</p>
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -27,16 +28,19 @@ export default {
   data() {
     return {
       results:[],
+      isLoading: false
     }
   },
   methods:{
     loadExperiences(){
+      this.isLoading = true
       fetch('https://vue-http-demo-a7128-default-rtdb.europe-west1.firebasedatabase.app/surveys.json').then((response) => {
         if (response.ok) {
         return  response.json()
         }
       }).then((data) => {
-        const results =[]
+        this.isLoading = false;
+        const results =[];
         for (const id in data) {
           results.push({id:id,name:data[id].name,rating: data[id].rating})
         }
